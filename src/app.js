4f57b9a61b2c566290e696dd14e91bee659e8252
Tuan Anh Ha <hatuananhvnhn@gmail.com>
2019-06-09 23:54:13 +0200

    set up app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 const publicDirectoryPath = path.join(__dirname, '../public');
 const viewPath = path.join(__dirname, '../templates/views'); // customize the path
@@ -96,6 +97,6 @@ app.get('*', (req, res) => { // 404 Page
     });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Serving...');
 });
